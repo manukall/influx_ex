f58fcf743466466492ca7d874119667386ee82c7
Manuel Kallenbach <mk@manukall.de>
2015-10-30 12:35:41 +0100

    better connection tests

--- test/influx_ex/connection_test.exs
+++ test/influx_ex/connection_test.exs
@@ -4,59 +4,73 @@ defmodule InfluxEx.ConnectionTest do
 
   @db_name "influx_ex_test_db"
 
-  test "create_db" do
+  setup do
     TestConnection.drop_db(@db_name)
+    :ok
+  end
+
+  test "create_db" do
    :ok = TestConnection.create_db(@db_name)
-   {:error, "database already exists"} = TestConnection.create_db(@db_name)
+   {:error, _} = TestConnection.create_db(@db_name)
   end
 
   test "drop_db" do
-    TestConnection.drop_db(@db_name)
     :ok = TestConnection.create_db(@db_name)
 
     :ok = TestConnection.drop_db(@db_name)
-    {:error, "database not found: #{@db_name}"} = TestConnection.drop_db(@db_name)
+    {:error, _} = TestConnection.drop_db(@db_name)
   end
 
   test "write" do
-    TestConnection.drop_db(@db_name)
+    minimal_point = %{measurement: "cpu",
+                      fields: %{load: 0.12}}
+
+    {:error, _} = TestConnection.write(minimal_point, @db_name)
+
+
     TestConnection.create_db(@db_name)
-    :ok = TestConnection.write(%{measurement: "cpu",
+
+    :ok = TestConnection.write(minimal_point, @db_name)
+    [{:ok, [result]}] = TestConnection.read("SELECT * FROM cpu", @db_name)
+    assert result[:series] == "cpu"
+    [%{"load" => 0.12}] = result[:points]
+
+    :ok = TestConnection.write(%{measurement: "series_2",
                                  fields: %{load: 0.12},
                                  tags: %{host: "web-staging"}},
                                @db_name)
+    [{:ok, [result]}] = TestConnection.read("SELECT * FROM series_2", @db_name)
+    [%{"host" => "web-staging"}] = result[:points]
 
-    :ok = TestConnection.write(%{measurement: "cpu",
-                                 fields: %{load: 0.12}},
-                               @db_name)
 
-    :ok = TestConnection.write(%{measurement: "cpu",
+    :ok = TestConnection.write(%{measurement: "series_3",
                                  fields: %{load: 0.12},
-                                 tags: %{host: "web-staging"},
                                  time: 12345678},
                                @db_name)
+    [{:ok, [result]}] = TestConnection.read("SELECT * FROM series_3", @db_name)
+    [%{"time" => "1970-01-01T00:00:00.012345678Z"}] = result[:points]
   end
 
   test "read" do
-    TestConnection.drop_db(@db_name)
-
-    [{:error, "database not found: #{@db_name}"}] = TestConnection.read("SELECT * FROM cpu", @db_name)
+    [{:error, _}] = TestConnection.read("SELECT * FROM cpu", @db_name)
 
     TestConnection.create_db(@db_name)
-    :ok = TestConnection.write(%{measurement: "cpu",
-                                 fields: %{load: 0 + 0.12},
-                                 tags: %{host: "web-staging"},
-                                 time: 12345678},
-                               @db_name)
+    for i <- 0..9 do
+      :ok = TestConnection.write(%{measurement: "cpu",
+                                   fields: %{load: i + 0.12},
+                                   tags: %{host: "web-staging"},
+                                   time: i + 12345678},
+                                 @db_name)
+    end
 
 
     [{:ok, results}] = TestConnection.read("SELECT * FROM cpu", @db_name)
 
-    assert results == [%{series: "cpu",
-                             points: [
-                               %{"time" => "1970-01-01T00:00:00.012345678Z",
-                                 "load" => 0.12,
-                                 "host" => "web-staging"}
-                             ]}]
+    [series_result] = results
+    assert series_result.series == "cpu"
+    assert length(series_result.points) == 10
+    assert hd(series_result.points) == %{"time" => "1970-01-01T00:00:00.012345678Z",
+                                         "load" => 0.12,
+                                         "host" => "web-staging"}
   end
 end
